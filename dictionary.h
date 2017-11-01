ce5dcdce7ded66f5398da4291430b2e5301d3286
Erik Hendriks <erik.hendriks@linuxmail.org>
2017-11-01 16:43:30 +0100

    Create dictionary.h

---
+++ dictionary.h
@@ -0,0 +1,40 @@
+/**
+ * Declares a dictionary's functionality.
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdbool.h>
+
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+
+typedef struct node
+{
+    struct node* nodes[27];
+    bool word;
+}node;
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word);
+
+/**
+ * Loads dictionary into memory. Returns true if successful else false.
+ */
+bool load(const char *dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+#endif // DICTIONARY_H
